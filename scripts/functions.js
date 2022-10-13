e42052497cc59dff879b00d6fbf9eb16e7998688
Isaac Sanchez <isaacsg20@gmail.com>
2022-10-13 18:58:09 +0200

    finalizados ejercicios

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,21 +1,30 @@
 'use strict'
 
 function addGrades(grades, newGrades) {
+	let notas = newGrades.split(',');
+	return grades.concat(notas);;
 }
 
 function clearGrades(grades) {
+	let newGrades = grades.map(nota => Number(nota));
+	return newGrades.filter(nota => (nota >= 0 && nota <= 10) && !isNaN(nota));
 }
 
 function firstFailed(grades) {
+	return grades.find(nota => nota < 5);
 }
 
 function passedGrades(grades) {
+	return grades.filter(nota => nota > 5);
 }
 
 function avgGrade(grades) {
+	const avg = grades.reduce((total,nota)  => total +  nota)
+	return (avg / grades.length).toFixed(2);
 }
 
 function finalGrades(grades, increment) {
+	return grades.map(nota  => (nota + (nota * increment) / 100).toFixed(0));
 }
 
 module.exports = {
